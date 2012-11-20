d9016e0774e69ccbd0e464fa57113b8d725a3163
Epicgrim <nathanlil13@me.com>
2012-11-20 14:50:50 -0500

    rails shell

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
-$:.unshift './lib'
-require 'app.rb'
+# This file is used by Rack-based servers to start the application.
 
-run Sinatra::Application
+require ::File.expand_path('../config/environment',  __FILE__)
+run Bidsy::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidsy::Application.config.secret_token = '87b00e6fc5e2d0f6e42203f4062d62b98051824c0c05698dbbe9056818866095fefecce1958b285adcf4e69af2e3f5a157b1bce371be60f1def711ec9f87205f'
