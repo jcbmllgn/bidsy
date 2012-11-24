2b84164e3c9320a6b4487a332cfa129080b8a29b
Epicgrim <nathanlil13@me.com>
2012-11-23 20:52:45 -0500

    editable helper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,12 +23,14 @@ class PostsController < ApplicationController
     if @user
       @post = @user.posts.new
 
-      # create 10 new images, this is the max number of images users
+      # create 1 new image, this is the max number of images users
       # can upload.
       1.times { @post.images << Image.new }
     else
       redirect_to login_path
     end
+
+    @editing = true
   end
 
   # Show a specific post.
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+
+  def editable(tag, *args, &block)
+    if block_given?
+      options      = args.first || {}
+      html_options = args.second
+      link_to(tag, capture(&block), options, html_options)
+    else
+      tag          = tag.to_s
+      content      = args[0]
+      html_options = args[1]
+
+      html_options[:class] = "editable"
+      html_options[:contenteditable] = "true"
+      tag_options = tag_options(html_options)
+
+      html = "<#{tag} #{tag_options}>#{ERB::Util.html_escape(content)}</#{tag}>"
+      html.html_safe
+    end
+  end
+
 end
