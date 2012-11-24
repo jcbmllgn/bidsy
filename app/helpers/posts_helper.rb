ccd2348316bdbdc5dee40af62f03f42b7149d01b
Epicgrim <nathanlil13@me.com>
2012-11-23 20:07:51 -0500

    Contenteditable changes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+
+  def editable_area( tag, id )
+    if block_given?
+      "<#{tag} class='editable' id='#{id}' contenteditable>" + yield  + "</#{tag}>".html_safe
+    else
+      "<#{tag} class='editable' id='#{id}' contenteditable></#{tag}>".html_safe
+    end
+  end
+
 end
