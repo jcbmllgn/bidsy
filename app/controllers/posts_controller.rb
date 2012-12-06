45b4671c60ba1b1587f4ae19f93e399005db7226
Nathan Lilienthal <nathanlil13@me.com>
2012-12-06 18:00:54 -0500

    new route for posting, and new files for post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,8 +26,6 @@ class PostsController < ApplicationController
       # create 1 new image, this is the max number of images users
       # can upload.
       1.times { @post.images << Image.new }
-
-      render :post
     else
       redirect_to login_path
     end
@@ -38,8 +36,6 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-
-    render :post
   end
 
   # Destory ond of the current users posts, only if nobody has
